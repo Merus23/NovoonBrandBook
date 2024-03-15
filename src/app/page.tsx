fd3c8357e52f2d69d4526fbf034574d81b1f30a0
Marcio Filho <marciofilho@live.com>
2024-03-14 21:38:43 -0300

    feat: add Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { log } from "console";
 import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+import logo_logo from "/public/assets/logo_logo.png";
 
 export default function Home() {
   return (
@@ -9,6 +11,33 @@ export default function Home() {
           { title: "HOME", link: "/home" },
           { title: "CONTACT", link: "/contact" },
           { title: "ABOUT", link: "/about" },
+        ]}
+      />
+      <Footer 
+        src={logo_logo} 
+        title="Essence"
+        items={[
+          {
+            title: "Resources",
+            content: [
+              { title: "Next", link: "https://nextjs.org/" },
+              { title: "Tailwind CSS", link: "https://tailwindcss.com/" }
+            ]
+          },
+          {
+            title: "Follow us",
+            content: [
+              { title: "Github", link: "#" },
+              { title: "Discord", link: "#" }
+            ]
+          },
+          {
+            title: "Legal",
+            content: [
+              { title: "Privacy Policy", link: "#" },
+              { title: "Terms & Conditions", link: "#" }
+            ]
+          }
         ]}
       />
     </>
