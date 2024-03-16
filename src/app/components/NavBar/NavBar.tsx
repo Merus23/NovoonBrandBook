f494da0910068ea4409829ccff92e2543d1a38b9
Merus23 <mateus.satira@gmail.com>
2024-03-15 21:32:45 -0300

    refactor: redo the NavBar component using tailwind

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,7 +1,10 @@
-import React, { ReactNode } from "react";
+"use client";
+
+import React, { ReactNode, useState } from "react";
 import styles from "./NavBar.module.css";
 import Link from "next/link";
 import Image, { StaticImageData } from "next/image";
+import { FaBars } from "react-icons/fa";
 
 type Item = {
   title: string;
@@ -14,25 +17,38 @@ type Props = {
 };
 
 function NavBar({ logo, items }: Props) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className={styles.navbar}>
+    <nav
+      className={
+        "bg-[#f8f9fa] flex justify-between py-4 px-8 items-center gap-8"
+      }
+    >
       <Link href="/">
-        <Image src={logo} alt="logo" className={styles.logo} />
+        <Image src={logo} alt="logo" className={"w-24"} />
       </Link>
 
-      <ul className={styles.navList}>
+      <ul className={"hidden md:flex flex-rows space-x-4"}>
         {items.map((item, index) => (
           <li>
-            <Link href={item.link}>{item.title}</Link>
+            <Link
+              href={item.link}
+              className="text-gray-700 hover:text-gray-900 scale-100 transition-tranform duration-700 hover:scale-150"
+            >
+              {item.title}
+            </Link>
           </li>
         ))}
       </ul>
 
-      <div className={styles.burger}>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+      <button onClick={handleMenu} className="block md:hidden">
+        <FaBars className="h-8 w-8 cursor-pointer" />
+      </button>
     </nav>
   );
 }
