8e34eb90012b60479c4a80dd8bc3fac036dbd3dc
marcospaulor <dev.silva.marcos@gmail.com>
2024-03-16 00:25:21 -0300

    feat: add more content and stylist to SideMenu.tsx

--- src/app/components/SideMenu/SideMenu.tsx
+++ src/app/components/SideMenu/SideMenu.tsx
@@ -1,57 +1,54 @@
-'use client'
+import React, { Dispatch, SetStateAction } from 'react';
+import Link from 'next/link';
 
-import React, { Dispatch, SetStateAction } from 'react'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+type MenuItem = {
+  label: string;
+  href: string;
+};
+
+type MenuSection = {
+  title?: string;
+  items: MenuItem[];
+};
 
 type Props = {
-  isOpen: boolean
-  onClose: Dispatch<SetStateAction<boolean>>
-}
+  isOpen: boolean;
+  onClose: Dispatch<SetStateAction<boolean>>;
+  menuSections: MenuSection[];
+};
 
-function SideMenu({isOpen, onClose}: Props) {
-  const pathname = usePathname()
-
+function SideMenu({ isOpen, onClose, menuSections }: Props) {
   return (
-    <div className={`fixed flex-col bg-gray-800 w-screen h-screen md:right-0 z-10 md:w-64 transition-transform duration-300 ${isOpen ? 'md:translate-x-64' && 'translate-x-full' : 'md:-translate-x-0' && '-translate-x-0:'}`}>
-      <div className="p-4 bg-gray-900 text-white">Menu</div>
-      <ul className="p-2">
-        <li className="text-white mb-2">
-          <Link href="/" className="hover:bg-gray-700 block px-4 py-2 rounded">Menu 1</Link>
-        </li>
-        <li className="text-white mb-2">
-          <Link href="/" className="hover:bg-gray-700 block px-4 py-2 rounded">Menu 2</Link>
-        </li>
-        <li className="text-white mb-2">
-          <Link href="/" className="hover:bg-gray-700 block px-4 py-2 rounded">Menu 3</Link>
-        </li>
-        <li className="text-white mb-2">
-          <Link href="/" className="hover:bg-gray-700 block px-4 py-2 rounded">Menu 4</Link>
-        </li>
-        <li className="text-white mb-2">
-          <Link href="/" className="hover:bg-gray-700 block px-4 py-2 rounded">Menu 4</Link>
-        </li>
-        <li className="text-white mb-2">
-          <Link href="/" className="hover:bg-gray-700 block px-4 py-2 rounded">Menu 4</Link>
-        </li>
-        <li className="text-white mb-2">
-          <Link href="/" className="hover:bg-gray-700 block px-4 py-2 rounded">Menu 4</Link>
-        </li>
-        <li className="text-white mb-2">
-          <Link href="/" className="hover:bg-gray-700 block px-4 py-2 rounded">Menu 4</Link>
-        </li>
-        <li className="text-white mb-2">
-          <Link href="/" className="hover:bg-gray-700 block px-4 py-2 rounded">Menu 4</Link>
+    <div className={`fixed flex-col bg-white text-black border-l border-black w-screen h-screen md:right-0 z-10 md:w-56 transition-transform duration-300 ${isOpen ? 'md:translate-x-64 translate-x-full ' : '-translate-x-0'}`}>
+      <ul className='h-full flex flex-col justify-between'>
+        {menuSections.map((section, sectionIndex) => (
+          <React.Fragment key={sectionIndex}>
+            {section.title && (
+              <li>
+                <span className="p-4 font-bold uppercase tracking-widest flex items-center justify-center md:justify-normal">{section.title}</span>
+              </li>
+            )}
+            {section.items.map((item, itemIndex) => (
+              <li key={itemIndex} className={`my-1 flex-1 inline-flex ${!section.title ? 'border-b border-black' : ''}`}>
+                <Link href={item.href} className="hover:-translate-y-2 w-full transition-transform duration-300 flex items-center justify-center md:justify-normal px-4 py-2 rounded">
+                  {item.label}
+                </Link>
+              </li>
+            ))}
+          </React.Fragment>
+        ))}
+        <li>
+          <button
+            onClick={() => onClose(false)}
+            className="flex flex-row justify-center md:justify-between items-center w-full h-full px-4 py-2 border-t border-black "
+          >
+            <span className="hover:-translate-y-2 transition-transform duration-300 tracking-widest">Close</span>
+            <span className='ml-4 text-3xl'>X</span>
+          </button>
         </li>
       </ul>
-      <button
-        onClick={() => onClose(false) }
-        className="bg-gray-900 w-full text-white p-2 rounded hover:bg-gray-700 transition-colors duration-300"
-      >
-        Close
-      </button>
     </div>
-  )
+  );
 }
 
-export default SideMenu
+export default SideMenu;
