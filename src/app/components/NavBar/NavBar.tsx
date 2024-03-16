cad9211a639e76a8c6235ea79b2db868147c149b
Merus23 <mateus.satira@gmail.com>
2024-03-16 17:32:52 -0300

    feat: add some responsive classnames in the navbar component

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -5,6 +5,7 @@ import styles from "./NavBar.module.css";
 import Link from "next/link";
 import Image, { StaticImageData } from "next/image";
 import { FaBars } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
 
 type Item = {
   title: string;
@@ -20,25 +21,33 @@ function NavBar({ logo, items }: Props) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const handleMenu = () => {
+    console.log("Passed here");
     setIsOpen(!isOpen);
   };
 
   return (
     <nav
-      className={
-        "bg-[#f8f9fa] flex justify-between py-4 px-8 items-center gap-8"
-      }
+      //bg-[#f8f9fa]
+
+      className={`  ${
+        isOpen
+          ? "bg-red-800 flex flex-col justify-between py-4 px-8 items-center gap-8  h-screen "
+          : "bg-blue-800 flex justify-between py-4 px-8 items-center gap-8"
+      }`}
     >
       <Link href="/">
-        <Image src={logo} alt="logo" className={"w-24"} />
+        <Image src={logo} alt="logo" className={"w-24 hidden sm:block"} />
       </Link>
 
-      <ul className={"hidden md:flex flex-rows space-x-4"}>
+      {/* "" */}
+      <ul className={`${isOpen ? "" : "hidden md:flex flex-rows space-x-4"}`}>
         {items.map((item, index) => (
           <li>
             <Link
               href={item.link}
-              className="inline-block text-gray-700 hover:text-gray-900 transform scale-100 transition-transform duration-700 hover:scale-110"
+              className={` "inline-block text-gray-700 hover:text-gray-900 transform scale-100 transition-transform duration-700 hover:scale-110"${
+                isOpen ? "" : ""
+              }`}
             >
               {item.title}
             </Link>
@@ -46,8 +55,15 @@ function NavBar({ logo, items }: Props) {
         ))}
       </ul>
 
-      <button onClick={handleMenu} className="block md:hidden">
-        <FaBars className="h-8 w-8 cursor-pointer" />
+      <button
+        onClick={handleMenu}
+        className={`block md:hidden ${isOpen ? "" : ""}`}
+      >
+        {isOpen ? (
+          <IoMdClose className="h-8 w-8 cursor-pointer " />
+        ) : (
+          <FaBars className="h-8 w-8 cursor-pointer" />
+        )}
       </button>
     </nav>
   );
