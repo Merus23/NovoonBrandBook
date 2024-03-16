dd52360b1917073a70e405b666d8eb4f95b29a87
marcospaulor <dev.silva.marcos@gmail.com>
2024-03-16 00:26:45 -0300

    feat: add new types of letterSpacing in the tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,16 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    letterSpacing: {
+      tightest: "-.075em",
+      tighter: "-.05em",
+      tight: "-.025em",
+      normal: "0",
+      wide: ".025em",
+      wider: ".05em",
+      widest: ".1em",
+      oneem: "1em",
+    },
   },
   plugins: [],
 };
