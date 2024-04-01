84fa5f0c45ebe92544cf1d11c9fe473efc79a943
marcospaulor <dev.silva.marcos@gmail.com>
2024-04-01 16:29:32 -0300

    fix: formatting issues in NavBar component

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,27 +1,27 @@
-"use client";
+'use client'
 
-import React, { ReactNode, useState } from "react";
-import Link from "next/link";
-import Image, { StaticImageData } from "next/image";
-import { FaBars } from "react-icons/fa";
-import { IoMdClose } from "react-icons/io";
+import React, { ReactNode, useState } from 'react'
+import Link from 'next/link'
+import Image, { StaticImageData } from 'next/image'
+import { FaBars } from 'react-icons/fa'
+import { IoMdClose } from 'react-icons/io'
 
 type Item = {
-  title: string;
-  link: string;
-};
+  title: string
+  link: string
+}
 
 type Props = {
-  logo: StaticImageData;
-  items: Item[];
-};
+  logo: StaticImageData
+  items: Item[]
+}
 
 function NavBar({ logo, items }: Props) {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false)
 
   const handleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+    setIsOpen(!isOpen)
+  }
 
   return (
     <nav
@@ -29,35 +29,37 @@ function NavBar({ logo, items }: Props) {
 
       className={`  ${
         isOpen
-          ? " flex flex-col justify-between py-4 px-8 items-center gap-8  h-screen "
-          : " flex justify-between py-4 px-8 items-center gap-8"
+          ? ' flex flex-col justify-between py-4 px-8 items-center gap-8  h-screen '
+          : ' flex justify-between py-4 px-8 items-center gap-8'
       }`}
     >
       <Link href="/">
         <Image
           src={logo}
           alt="logo"
-          className={`${isOpen ? "w-24 hidden" : "w-24 block"}`}
+          className={`${isOpen ? 'w-24 hidden' : 'w-24 block'}`}
         />
       </Link>
 
       <ul
         className={` md:flex flex-row gap-10 ${
           isOpen
-            ? "flex-col space-y-5 w-full justify-center items-center"
-            : "hidden "
+            ? 'flex-col space-y-5 w-full justify-center items-center'
+            : 'hidden '
         }`}
       >
         {items.map((item, index) => (
           <li
+            key={index}
             className={` ${
-              isOpen ? "border-y-2 p-6 w-full flex justify-center" : ""
+              isOpen ? 'border-y-2 p-6 w-full flex justify-center' : ''
             }`}
           >
             <Link
+              key={index}
               href={item.link}
               className={` inline-block text-gray-700 hover:text-gray-900 transform scale-100 transition-transform duration-700 hover:scale-110 ${
-                isOpen ? "" : ""
+                isOpen ? '' : ''
               }`}
             >
               {item.title}
@@ -68,18 +70,18 @@ function NavBar({ logo, items }: Props) {
 
       <button
         onClick={handleMenu}
-        className={`  ${isOpen ? "block" : "md:hidden"}`}
+        className={`  ${isOpen ? 'block' : 'md:hidden'}`}
       >
         {isOpen ? (
           <IoMdClose
-            className={`h-8 w-8 cursor-pointer  ${isOpen ? "" : ""}`}
+            className={`h-8 w-8 cursor-pointer  ${isOpen ? '' : ''}`}
           />
         ) : (
           <FaBars className="h-8 w-8 cursor-pointer" />
         )}
       </button>
     </nav>
-  );
+  )
 }
 
-export default NavBar;
+export default NavBar
